e921465bba2060df01f99298f747053c827eca9e
wittycrypto <86157531+wittycrypto@users.noreply.github.com>
2023-09-04 11:35:17 +0800

    Add files via upload

--- style.js
+++ style.js
@@ -1,9 +1,24 @@
-$('#carouselModal').on('show.bs.modal', function () {
-    $('.modal-dialog').addClass('modal-fullscreen');
-  });
-  
-  $('#carouselModal').on('hidden.bs.modal', function () {
-    $('.modal-dialog').removeClass('modal-fullscreen');
-  });
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:8
+        }
+    }
+})
 
-  
+$('#carouselModal').on('show.bs.modal', function () {
+$('.modal-dialog').addClass('modal-fullscreen');
+});
+
+$('#carouselModal').on('hidden.bs.modal', function () {
+$('.modal-dialog').removeClass('modal-fullscreen');
+});
